efdbc1844b04390e614f982da0a567b8f657fe8a
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2025-08-13 08:37:11 -0500

    Update and rename Acti1.java to Actividad1.java

---
+++ GUIA 1/Actividades/Actividad1/Actividad1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class NumerosPrimos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce un numero: ");
+        int n = scanner.nextInt();
+        imprimirNumerosPrimos(n);
+        verificarNumeroIngresado(n); // Verificamos el número ingresado
+    }
+
+    public static boolean esPrimo(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void imprimirNumerosPrimos(int n) {
+        for (int num = 2; num <= n; num++) {
+            if (esPrimo(num)) {
+                System.out.println(num + " es primo.");
+            }
+        }
+    }
+
+    public static void verificarNumeroIngresado(int n) {
+        if (esPrimo(n)) {
+            System.out.println("El número ingresado " + n + " es primo.");
+        } else {
+            System.out.println("El número ingresado " + n + " no es primo.");
+        }
+    }
+}
