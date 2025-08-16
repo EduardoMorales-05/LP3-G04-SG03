8856b9fbd1e017edd922ee1ee0e3320300cb01cc
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2025-08-16 09:34:38 -0500

    Create Main.java

---
+++ GUIA 1/Actividades/Actividad5/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5}; // Declaración con tipo
+        
+        // Imprimir
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+
+/* C++
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5}; // Tamaño inferido
+    
+    // Imprimir
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+    
+}*/
+
+/* Python
+arr = [1, 2, 3, 4, 5]  # Lista (no requiere tipo)
+
+# Imprimir
+for num in arr:
+    print(num, end=" ") */
