de6e712c8c29af17f152b7d5a31269bc04253a0c
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2025-08-24 01:46:42 -0500

    Create Contador.java

---
+++ GUIA 2/Actividades/Actividad 2/Contador.java
@@ -0,0 +1,39 @@
+public class Contador {
+    static int acumulador = 0;
+    private int valor;
+    final static int VALOR_INICIAL = 10;
+    static int nContadores = 0;
+    static int ultimoContador = 0;
+
+    public static int acumulador() {
+        return acumulador;
+    }
+
+    public static int getNContadores() {
+        return nContadores;
+    }
+
+    public static int getUltimoContador() {
+        return ultimoContador;
+    }
+
+    public Contador(int valor) {
+        this.valor = valor;
+        acumulador += valor;
+        nContadores++;
+        ultimoContador = valor;
+    }
+
+    public Contador() {
+        this(Contador.VALOR_INICIAL);
+    }
+
+    public void inc() {
+        valor++;
+        acumulador++;
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+}
