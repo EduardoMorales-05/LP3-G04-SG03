212aa305af0daff8bbd130192340ae198cce85ed
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2025-09-14 20:55:45 -0500

    Create Calculadora.java

---
+++ GUIA 4/Ejercicios/Ejercicio2/Calculadora.java
@@ -0,0 +1,23 @@
+package exp;
+
+public class Calculadora {
+
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public double restar(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) throws DivisionPorCeroException {
+        if (b == 0) {
+            throw new DivisionPorCeroException("No se puede dividir entre cero.");
+        }
+        return a / b;
+    }
+}
