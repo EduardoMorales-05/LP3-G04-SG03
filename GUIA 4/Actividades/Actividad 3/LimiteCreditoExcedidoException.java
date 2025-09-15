f79f75c4de441d57f81e257da5fdcc83d611f32c
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2025-09-14 20:21:37 -0500

    Create LimiteCreditoExcedidoException.java

---
+++ GUIA 4/Actividades/Actividad 3/LimiteCreditoExcedidoException.java
@@ -0,0 +1,7 @@
+package exp;
+
+public class LimiteCreditoExcedidoException extends RuntimeException {
+    public LimiteCreditoExcedidoException(String mensaje) {
+        super(mensaje);
+    }
+}
