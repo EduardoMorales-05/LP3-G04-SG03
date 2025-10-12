ddee0388637bf9274b0fd3d89f030a36370891ca
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2025-10-12 12:24:45 -0500

    Create Persona.java

---
+++ GUIA 7/Actividades/Actividad 5/Persona.java
@@ -0,0 +1,30 @@
+package defaul;
+
+public class Persona {
+    private String nombre;
+    private String telefono;
+    private String direccion;
+    
+    public Persona(String nombre, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getTelefono() {
+        return telefono;
+    }
+    
+    public String getDireccion() {
+        return direccion;
+    }
+    
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "\nTeléfono: " + telefono + "\nDirección: " + direccion + "\n";
+    }
+}
