a352b5ad8aa99751b8a628ff4a3153880330af4a
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2025-10-12 14:54:26 -0500

    Create MainEmpleados.java

---
+++ GUIA 7/Ejercicios/Ejercicio 3/main/MainEmpleados.java
@@ -0,0 +1,10 @@
+package main;
+
+import vista.EmpleadoView;
+
+public class MainEmpleados {
+    public static void main(String[] args) {
+        EmpleadoView vista = new EmpleadoView();
+        vista.iniciar();
+    }
+}
