810f995ac37e43efe57a1ea6513ec0bc558424e6
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2025-10-12 12:24:09 -0500

    Create AppContactos.java

---
+++ GUIA 7/Actividades/Actividad 5/AppContactos.java
@@ -0,0 +1,10 @@
+package defaul;
+
+public class AppContactos {
+    public static void main(String[] args) {
+        System.out.println("=== INICIANDO SISTEMA DE AGENDA ===");
+        Agenda agenda = new Agenda();
+        agenda.bucle();
+        System.out.println("=== SISTEMA FINALIZADO ===");
+    }
+}
