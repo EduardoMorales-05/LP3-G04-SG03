8433a2a1f0f93ef476dc335697d4b62374ecb6c7
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2025-11-22 21:47:49 -0500

    Create Producto.java

---
+++ GUIA 10/Ejercicios/Ejercicio1/Producto.java
@@ -0,0 +1,48 @@
+package swingdemoo;
+
+public class Producto {
+    private String nombre;
+    private double precio;
+    private int cantidadStock;
+    private String categoria;
+
+    public Producto(String nombre, double precio, int cantidadStock, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidadStock = cantidadStock;
+        this.categoria = categoria;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setCantidadStock(int cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
